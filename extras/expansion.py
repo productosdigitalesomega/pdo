2a2cea69769b778bff08c68e21d4d737cf995181
productosdigitalesomega <productosdigitalesomega@gmail.com>
2025-01-13 23:00:12 -0600

    Update expansion.py

--- extras/expansion.py
+++ extras/expansion.py
@@ -50,7 +50,7 @@ class FooocusExpansion:
                 self.logits_bias[0, v] = 0
                 debug_list.append(k[1:])
 
-        print(f'Fooocus V2 Expansion: Vocab with {len(debug_list)} words.')
+        #print(f'Fooocus V2 Expansion: Vocab with {len(debug_list)} words.')
 
         # debug_list = '\n'.join(sorted(debug_list))
         # print(debug_list)
@@ -76,7 +76,7 @@ class FooocusExpansion:
             self.model.half()
 
         self.patcher = ModelPatcher(self.model, load_device=load_device, offload_device=offload_device)
-        print(f'Fooocus Expansion engine loaded for {load_device}, use_fp16 = {use_fp16}.')
+        #print(f'Fooocus Expansion engine loaded for {load_device}, use_fp16 = {use_fp16}.')
 
     @torch.no_grad()
     @torch.inference_mode()
@@ -97,7 +97,7 @@ class FooocusExpansion:
             return ''
 
         if self.patcher.current_device != self.patcher.load_device:
-            print('Fooocus Expansion loaded by itself.')
+            #print('Fooocus Expansion loaded by itself.')
             model_management.load_model_gpu(self.patcher)
 
         seed = int(seed) % SEED_LIMIT_NUMPY
